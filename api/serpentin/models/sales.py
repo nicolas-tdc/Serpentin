03aa9d8056b1b8dba8d1f80c7dee57c7b247e40d
nicolas.tdc <nicolas.tachetdescombes@gmail.com>
2022-01-16 22:27:59 +0100

    Sales models

--- api/serpentin/models/sales.py
+++ api/serpentin/models/sales.py
@@ -7,3 +7,8 @@ class Sales(db.Entity):
     name = Required(str)
     deals = Set("Deal")
     statements = Set("Statement")
+
+    def get_formatted_data(self) -> dict:
+        return {
+            "name": self.name,
+        }
