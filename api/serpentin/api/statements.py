cd052d01b489be30c9159ca0a7924bec31bafa49
nicolas.tdc <nicolas.tachetdescombes@gmail.com>
2022-01-21 21:19:42 +0100

    Remove redundant statements by sales API view

--- api/serpentin/api/statements.py
+++ api/serpentin/api/statements.py
@@ -2,7 +2,6 @@ from flask_restful import Resource
 
 from serpentin.api import api
 from serpentin.managers.statements import get_statements, get_statements_by_sales
-from serpentin.managers.sales import Sales
 
 
 class Statements(Resource):
@@ -16,17 +15,3 @@ class Statements(Resource):
 
 
 api.add_resource(Statements, "/statements")
-
-
-class StatementsBySales(Resource):
-    def get(self, sales_id):
-        """Return statements by sales"""
-
-        sales = Sales[sales_id]
-        statements = get_statements_by_sales(sales)
-        formatted_statements = [statement.get_formatted_data() for statement in statements]
-
-        return {"statement": formatted_statements}
-
-
-api.add_resource(StatementsBySales, "/statements/<int:sales_id>")
