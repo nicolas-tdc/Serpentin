bfb171d2a12eff34f695dffa072113fa21d8bfdd
nicolas.tdc <nicolas.tachetdescombes@gmail.com>
2022-01-22 00:08:23 +0100

    Enable and configure pytest-ponyorm

--- api/serpentin/core/database.py
+++ api/serpentin/core/database.py
@@ -3,7 +3,22 @@ from pony.orm import Database
 db = Database()
 
 
+# Test database
+
+db.bind(provider="sqlite", filename="../tests/database.sqlite", create_db=True)
+
+from serpentin.models import compensations
+from serpentin.models import deals
+from serpentin.models import sales
+from serpentin.models import statements
+
+db.generate_mapping(create_tables=True)
+
+
+# Provision database
+
 def connect_to_database():
+    db.provider = db.schema = None
     db.bind(provider="sqlite", filename="./database.sqlite", create_db=True)
 
     from serpentin.models import compensations
